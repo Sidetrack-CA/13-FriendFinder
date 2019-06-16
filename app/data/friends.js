9ef0b59a34afd9523fa56a14e2d570063a2cbdfd
Rob Ross <rob@Home-iMac.local>
2019-06-15 21:09:06 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Rachel Zirin",
+		photo: "https://media.licdn.com/dms/image/C4E03AQGWOek6jloisw/profile-displayphoto-shrink_200_200/0?e=1564617600&v=beta&t=CYha78oxiwx-5XJ-nZIs7Ec_MMipb-pK1SDs9159SOA",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Olaf Funke",
+		photo: "https://i.ytimg.com/vi/OZsS6ImS6K8/maxresdefault.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Steve Wozniak",
+		photo: "https://jakewattenphul.files.wordpress.com/2014/01/steve-wozniak-apple-co-founder-segway-polo.jpg?w=267",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,28 @@
+
+// DEPENDENCIES
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+// EXPRESS CONFIGURATION
+// Tells node that we are creating an "express" server
+var app = express();
+
+// sets both a dynamic port for heroku and a static port for 127.0.0.1
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express middleware to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// ROUTER
+// This is where we link the apiroutes file to handle the routing of get requests
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+
+// SERVER PORT LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
